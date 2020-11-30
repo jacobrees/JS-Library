51b1b028826701d3233fdeddace5c801f5ea4374
Bello S Babakolo <fleetbeekay@gmail.com>
2020-11-30 15:36:30 +0100

    Implement display books to the page

--- library.js
+++ library.js
@@ -1,9 +1,32 @@
 let myLibrary = [];
 
-function Book(title) {
-  this.title = title
+function Book(title, author, numberOfPages, hasRead) {
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.hasRead = hasRead;
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book)
+  return myLibrary.push(book)
 }
+
+function bookCard(book) {
+  return `<div>
+    <h3>${book.title}</h3>
+    <p><span>Written by:</span> ${book.author}</p>
+    <p>Number of pages: ${book.numberOfPages}</p>
+    <p>${ book.hasRead ? "You have read this book" : "You haven't read this book!"} </p>
+  </div>`
+}
+
+function displayBooks() {
+  let booksList = ""
+  myLibrary.forEach( (book) => {
+    booksList += bookCard(book);
+  });
+
+  const booksContainer = document.getElementById("booksContainer");
+  booksContainer.innerHTML = booksList;
+}
+
