26650de736466792bf75d35fb8c057f15280e2fd
Bello S Babakolo <fleetbeekay@gmail.com>
2020-12-02 14:02:07 +0100

    Es6ify and fix linting error

--- library.js
+++ library.js
@@ -1,97 +1,100 @@
-let myLibrary = [];
+const myLibrary = [];
 
-function Book(title, author, numberOfPages, hasRead) {
-  this.title = title;
-  this.author = author;
-  this.numberOfPages = numberOfPages;
-  this.hasRead = hasRead;
+class Book {
+  constructor(title, author, numberOfPages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.hasRead = hasRead;
+  }
+
+  toggle() {
+    this.hasRead = !this.hasRead;
+  }
 }
 
-Book.prototype.toggle = function () {
-  this.hasRead = !this.hasRead;
-}
-
-function visibility(e){
-  let bookForm = document.forms.bookForm ;
+const visibility = (e) => {
+  const { bookForm } = document.forms;
   e.target.classList.add('hide-form');
-  bookForm.classList.remove("hide-form");
-  bookForm.classList.add("show-form");
-}
+  bookForm.classList.remove('hide-form');
+  bookForm.classList.add('show-form');
+};
 
-document.getElementById("addBook").addEventListener("click", visibility);
-
-function addBookToLibrary(book) {
+const addBookToLibrary = (book) => {
   myLibrary.push(book);
-}
+};
 
-function removeBookFromLibrary(e) {
-  let bookIndex = e.target.dataset.bookIndex;
-  myLibrary.splice(bookIndex, 1);
-  displayBooks();
-}
-
-function readBookToggler(e) {
-  let bookIndex = e.target.dataset.toggledBookIndex;
-  let book = myLibrary[bookIndex];
+const readBookToggler = (e) => {
+  const bookIndex = e.target.dataset.toggledBookIndex;
+  const book = myLibrary[bookIndex];
   book.toggle();
-}
+};
 
-function bookCard(book, bookIndex) {
-  return `<div>
+const bookCard = (book, bookIndex) => `<div>
     <h3>${book.title}</h3>
     <p><span>Written by:</span> ${book.author}</p>
     <p>Number of pages: ${book.numberOfPages}</p>
     <p>Read: 
-       <input type="checkbox" name="read" class="toggle-read" ${book.hasRead ? "checked" : ""} data-toggled-book-index="${bookIndex}" >
+       <input type="checkbox" name="read" class="toggle-read" ${book.hasRead ? 'checked' : ''} data-toggled-book-index="${bookIndex}" >
     </p>
     <button class="remove-btn" data-book-index="${bookIndex}">Remove Book</button>
-  </div>`
-}
+  </div>`;
 
-function displayBooks() {
-  let booksList = ""
-  myLibrary.forEach( (book, bookIndex) => {
+const displayBooks = () => {
+  let booksList = '';
+
+  const removeBookFromLibrary = (e) => {
+    const { bookIndex } = e.target.dataset;
+    myLibrary.splice(bookIndex, 1);
+    displayBooks();
+  };
+
+  myLibrary.forEach((book, bookIndex) => {
     booksList += bookCard(book, bookIndex);
   });
 
-  const booksContainer = document.getElementById("booksContainer");
+  const booksContainer = document.getElementById('booksContainer');
   booksContainer.innerHTML = booksList;
 
   const removeBookButtons = document.querySelectorAll('.remove-btn');
-  removeBookButtons.forEach ((btn) => { 
+  removeBookButtons.forEach((btn) => {
     btn.addEventListener('click', removeBookFromLibrary);
   });
 
-  const readTogglers = document.querySelectorAll(`.toggle-read`);
+  const readTogglers = document.querySelectorAll('.toggle-read');
   readTogglers.forEach((btn) => {
     btn.addEventListener('click', readBookToggler);
   });
-}
+};
 
-
-displayBooks();
-
-const bookForm = document.forms["bookForm"];
-
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const [title, author, numberOfPages, hasRead] = e.target.elements;
-  const book = new Book(title.value, author.value, numberOfPages.value, hasRead.checked);
-
-  addBookToLibrary(book);
-
+const jsLibrary = () => {
   displayBooks();
 
-  e.target.reset();
+  const { bookForm } = document.forms;
 
-  e.target.classList.remove('show-form');
-  e.target.classList.add('hide-form');
-  
-  let addBook = document.getElementById("addBook");
-  addBook.classList.remove('hide-form');
-  addBook.classList.add('show-form');
+  document.getElementById('addBook').addEventListener('click', visibility);
 
-  return false;
-});
+  bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    const [title, author, numberOfPages, hasRead] = e.target.elements;
+    const book = new Book(title.value, author.value, numberOfPages.value, hasRead.checked);
+
+    addBookToLibrary(book);
+
+    displayBooks();
+
+    e.target.reset();
+
+    e.target.classList.remove('show-form');
+    e.target.classList.add('hide-form');
+
+    const addBook = document.getElementById('addBook');
+    addBook.classList.remove('hide-form');
+    addBook.classList.add('show-form');
+
+    return false;
+  });
+};
+
+jsLibrary();
